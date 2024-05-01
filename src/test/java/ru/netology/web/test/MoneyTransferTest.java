c0bdb1bc3e0f471c8ceee6178bbb62bd14c19c53
mega test <mahp0@mail.ru>
2024-05-01 08:39:10 +0300

    headless set

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,10 +1,15 @@
 package ru.netology.web.test;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.LoginPage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -22,7 +27,16 @@ public class MoneyTransferTest {
 
     @BeforeEach
     void setup() {
-        var loginPage = open("http://localhost:9999", LoginPage.class);
+     //   ChromeOptions options = new ChromeOptions();
+      //  options.addArguments("--start-maximized");
+      //  Map<String, Object> prefs = new HashMap<String, Object>();
+      //  prefs.put("credentials_enable_service", false);
+       // prefs.put("password_manager_enabled", false);
+      //  options.setExperimentalOption("prefs", prefs);
+      //  Configuration.browserCapabilities = options;
+
+
+            var loginPage = open("http://localhost:9999", LoginPage.class);
         var authInfo = getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = getVerificationCode();
